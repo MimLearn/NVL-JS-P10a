fa5a1c7ee58f376e864b686099c1d20c7d7151a7
MimLearn <51676988+MimLearn@users.noreply.github.com>
2019-06-25 12:11:15 +0200

    p10a.js

---
+++ p10a.js
@@ -0,0 +1,13 @@
+<script>
+/*Dados los siguientes valores en dos variables de cadena:
+    Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo
+    Madrid.Sevilla.Barcelona.Valencia.Zaragoza.Bilbao.Donosti
+Crea un array donde se guarde en cada celda como primer elemento el día de la semana y como segundo el lugar donde se realiza el concierto.*/
+
+var diasComa = "Lunes,Martes,Miercoles,Jueves,Viernes,Sabado,Domingo";
+var lugaresPunto = "Madrid.Sevilla.Barcelona.Valencia.Zaragoza.Bilbao.Donosti";
+var diasComa = diasComa.split(",");
+var lugaresPunto = diasPunto.split(".");
+var matrizOrdenada = ([diasComa[0],lugaresPunto[0]],[diasComa[1],lugaresPunto[1]],[diasComa[2],lugaresPunto[2]],[diasComa[3],lugaresPunto[3]],
+[diasComa[4],lugaresPunto[4]],[diasComa[5],lugaresPunto[5]],[diasComa[6],lugaresPunto[6]]);
+</script>
